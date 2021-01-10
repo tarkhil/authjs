2149df340e8efc719d8a4e6afd880047d5939f15
Alex Povolotsky <tarkhil@over.ru>
2021-01-10 19:49:48 +0300

    Login works; but we gets main page displayed briefly before redirect to /login
    Besides than, we need to logout somehow

--- src/main.js
+++ src/main.js
@@ -2,11 +2,24 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import firebase from "firebase/app";
+import "firebase/auth";
+import firebaseAppConfig from "./firebaseauth";
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
-  store,
+    store,
+    created() {
+	firebase.initializeApp(firebaseAppConfig);
+	firebase.auth().onAuthStateChanged((user) => {
+	    if(user) {
+		this.$router.push('/')
+	    } else {
+		this.$router.push('/login')
+	    }
+	});
+    },
   render: h => h(App)
 }).$mount("#app");
