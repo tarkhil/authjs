a4c0469b06f3c9838294d1597197552d85ed11a2
Alex Povolotsky <tarkhil@over.ru>
2021-01-11 02:55:42 +0300

    MOSTLY working. Still tries to show main screen early

--- src/main.js
+++ src/main.js
@@ -1,13 +1,11 @@
 import Vue from "vue";
 import App from "./App.vue";
+import router from "./router";
+import { AuthGuard } from "vue-firebase-auth-plugins";
 import store from "./store";
 import firebase from "firebase/app";
 import "firebase/auth";
 import firebaseAppConfig from "./firebaseauth";
-firebase.initializeApp(firebaseAppConfig);
-import router from "./router";
-import { AuthGuard } from "vue-firebase-auth-plugins";
-Vue.use(AuthGuard, { auth: firebase.auth(), router: router, postAuthPath:"/" });
 
 Vue.config.productionTip = false;
 
@@ -15,13 +13,8 @@ new Vue({
   router,
     store,
     created() {
-	firebase.auth().onAuthStateChanged((user) => {
-	    if(user) {
-		this.$router.push('/')
-	    } else {
-		this.$router.push('/login')
-	    }
-	});
+	firebase.initializeApp(firebaseAppConfig);
+	AuthGuard.install(router, { auth: firebase.auth(), options: {postAuthPath: "/" }});
     },
   render: h => h(App)
 }).$mount("#app");
